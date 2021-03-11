6cf21af791e29f45c0e348512dfd2a9a0d5735c4
matiaguilera <73367049+matiaguilera@users.noreply.github.com>
2021-03-11 20:40:51 -0300

    First version of application moved into github

---
+++ models/author.js
@@ -0,0 +1,50 @@
+var mongoose = require('mongoose');
+
+const { DateTime } = require("luxon");
+
+var Schema = mongoose.Schema;
+
+var AuthorSchema = new Schema(
+  {
+    first_name: {type: String, required: true, maxlength: 100},
+    family_name: {type: String, required: true, maxlength: 100},
+    date_of_birth: {type: Date},
+    date_of_death: {type: Date},
+  }
+);
+
+// Virtual for author's full name
+AuthorSchema
+.virtual('name')
+.get(function () {
+  return this.family_name + ', ' + this.first_name;
+});
+
+// Virtual for author's lifespan
+AuthorSchema
+.virtual('lifespan')
+.get(function () {
+  if (this.date_of_birth && this.date_of_death)
+    return DateTime.fromJSDate(this.date_of_birth).toLocaleString(DateTime.DATE_MED) + " - " + DateTime.fromJSDate(this.date_of_death).toLocaleString(DateTime.DATE_MED);
+  else if(this.date_of_birth)
+    return DateTime.fromJSDate(this.date_of_birth).toLocaleString(DateTime.DATE_MED);
+  else
+    return null;
+  });
+
+//Virtual for formatted author's date of birth
+AuthorSchema
+.virtual('date_of_birth_formatted')
+.get(function () {
+  return DateTime.fromJSDate(this.date_of_birth).toLocaleString(DateTime.DATE_MED);
+});
+
+// Virtual for author's URL
+AuthorSchema
+.virtual('url')
+.get(function () {
+  return '/catalog/author/' + this._id;
+});
+
+//Export model
+module.exports = mongoose.model('Author', AuthorSchema);
